97d8f80b6138b7e639be4b3710663ff9d02d4ed6
User <txbroq90@gmail.com>
2022-02-08 11:43:20 -0500

    add dynamic GET route

--- server.js
+++ server.js
@@ -6,14 +6,21 @@ const mockUserData=[
         {name:'Jill'}   
 ]
 
-app.get('/user',funstion(req,res){
+app.get('/users',function(req,res){
     res.json({
             success: true,
             message: 'successfully got users. Nice!',
-            users: mockUserDate
+            users: mockUserData
             })
     })
-    
-app.listen(8000,function(){
-    console.log("server is running")
-})
+
+    app.get('/users/:id',function(req,res){
+        console.log(req,params.id)
+        res.json({
+            success: true,
+            message: 'got one user',
+            users: req.params.id
+        })
+    })
+
+app.listen(8000,function(){console.log('server is listening')})
