010f7a157be4df8aeacd5eae3a0a7f69b253f08f
User <txbroq90@gmail.com>
2022-02-08 12:01:32 -0500

    add login POST route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { application } = require('express');
 const express = require('express');
 const app = express();
 
@@ -21,6 +22,30 @@ app.get('/users',function(req,res){
             message: 'got one user',
             users: req.params.id
         })
+    })
+
+    application.post('/login',function(req,res){
+        //tcypically passwords are encrypted using somehting like bcrypyt before sending to datbase
+        const username=req.body.username;
+        const password=req.body.password;
+
+        //this shoule come from database
+        const mockUsername="billyTheKid";
+        const mockPassword="superSecret";
+
+        if (username===mockUsername && password===mockPassword){
+            //in practice, use json web token sign method to make an encrypted token
+            res.json({
+                success: true,
+                message: 'password and username match!',
+                token: 'encrypted tokwn goes here'
+            })
+        } else {
+            res.json({
+                success: false,
+                message: 'password and username dont match'
+            })
+        }
     })
 
 app.listen(8000,function(){console.log('server is listening')})
